e0c9902ec8cedc53373add2fe92d836c175b7fb3
DESKTOP-5MSA37D\dhavamba <dhavamba@gmail.com>
2017-07-29 22:27:49 +0200

    added camera

---
+++ Assets/_Game_/Scripts/SettingCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Com.LuisPedroFonseca.ProCamera2D;
+
+public class SettingCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake ()
+    {
+        ProCamera2D cam = GetComponent<ProCamera2D>();
+        Transform pl = GameObject.FindGameObjectWithTag("Player").transform;
+
+        cam.AddCameraTarget(pl);
+	}
+
+}
