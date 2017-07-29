7e1edd542569e467e1509ff8d399b2b6c4ccd370
Boros93 <lamp9300@yahoo.it>
2017-07-29 23:12:56 +0200

    EventTriggerFix

--- Assets/_Game_/Scripts/EventTrigger.cs
+++ Assets/_Game_/Scripts/EventTrigger.cs
@@ -7,6 +7,7 @@ public class EventTrigger : MonoBehaviour {
     //public typesOfEvents typeOfEvent;
     public EventManager manager;
     public GameObject objectToActivate;
+    
     public enum TypesOfEvent
     {
         Porta, Stampa
@@ -14,16 +15,46 @@ public class EventTrigger : MonoBehaviour {
 
     public TypesOfEvent evento;
 
+    bool isOnTrigger = false;
+    bool isPressed = false;
+
+    float timer = .5f;
+    float timeToRemaing;
+
     void Start()
     {
         manager = GameObject.FindGameObjectWithTag("EventManager").GetComponent<EventManager>();
     }
 
-    void OnTriggerStay2D(Collider2D other)
+    void FixedUpdate()
     {
-        if(other.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
+        if(timeToRemaing<=0)
+        {
+            isPressed = false;
+        }
+        if(!isPressed && isOnTrigger && Input.GetKeyDown(KeyCode.E))
         {
             manager.runEvent(evento.ToString(), objectToActivate);
+            isPressed = true;
+            timeToRemaing = timer;
+        }
+        timeToRemaing -= Time.fixedDeltaTime;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            isOnTrigger = true;
         }
     }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isOnTrigger = false;
+        }
+    }
+
 }
