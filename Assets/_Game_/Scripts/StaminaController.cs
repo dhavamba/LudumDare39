c6e8ca87ee7e2968e07b92d0a5d1b836973c7893
Boros93 <lamp9300@yahoo.it>
2017-07-29 16:36:08 +0200

    Stamina controller

---
+++ Assets/_Game_/Scripts/StaminaController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaminaController : MonoBehaviour
+{ 
+    public float MAX_STAMINA;
+    public float addStaminaSpeed;
+    public float subStaminaSpeed;
+
+    float timer = 1;
+    float timeToRemaining;
+
+    float stamina;
+    bool isAddStamina = false; //Devo ricaricare stamina?
+    
+    void Start()
+    {
+        stamina = MAX_STAMINA;
+    }
+
+    void Update()
+    {
+        if (timeToRemaining <= 0)
+        {
+            if (!isAddStamina && stamina > 0)
+            {
+                stamina -= subStaminaSpeed;
+                Debug.Log("Perdo stamina...  Stamina attuale: " + stamina);
+            }
+            else if(stamina < MAX_STAMINA)
+            {
+                stamina += addStaminaSpeed;
+
+                if(stamina>MAX_STAMINA)
+                {
+                    stamina = MAX_STAMINA;
+                }
+
+                Debug.Log("Ricarico stamina...  Stamina attuale: " + stamina);
+            }
+            timeToRemaining = timer;
+        }
+
+        timeToRemaining -= Time.deltaTime;
+    }
+
+    public void setStamina(bool value)//Se passo true ricarico, se passo false scarico
+    {
+        isAddStamina = value;
+    }
+}
---
+++ Assets/_Game_/Scripts/ZoneEventTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneEventTrigger : MonoBehaviour {
+
+    public StaminaController stmController;
+    public bool isRescueZone = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !isRescueZone)
+        {
+            stmController.setStamina(true);
+        }
+        else if (other.CompareTag("Player") && isRescueZone)
+        {
+            stmController.setStamina(false);
+        }
+    }
+}
